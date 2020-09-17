21bd50f24e55a17752be463076c6ac9ba5061ec0
Max <bludux595@gmail.com>
2020-09-17 13:03:11 -0400

    Changed redstone jellyfish texture and modified gold and iron jellyfish drop times

--- src/main/java/blueduck/morejellyfish/morejellyfishmod/entity/IronJellyfishEntity.java
+++ src/main/java/blueduck/morejellyfish/morejellyfishmod/entity/IronJellyfishEntity.java
@@ -12,6 +12,6 @@ import net.minecraft.world.World;
 
 public class IronJellyfishEntity extends AbstractJellyfishEntity {
     public IronJellyfishEntity(EntityType<? extends AbstractFishEntity> type, World worldIn) {
-        super(type, worldIn, new ItemStack((IItemProvider) MoreJellyfishItems.IRON_JELLYFISH.get(), 1), (Item) Items.IRON_NUGGET, 1.0D, true, 300, 4, 0.4D, 0.3D, 0.15D);
+        super(type, worldIn, new ItemStack((IItemProvider) MoreJellyfishItems.IRON_JELLYFISH.get(), 1), (Item) Items.IRON_NUGGET, 2.0D, true, 300, 4, 0.4D, 0.3D, 0.15D);
     }
 }
