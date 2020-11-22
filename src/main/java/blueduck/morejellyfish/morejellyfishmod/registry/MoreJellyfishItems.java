41b200485543ea098a632ba96f1334ff8745a892
Max <bludux595@gmail.com>
2020-11-21 20:50:14 -0500

    adds netherite jellyfish and associated recipes

--- src/main/java/blueduck/morejellyfish/morejellyfishmod/registry/MoreJellyfishItems.java
+++ src/main/java/blueduck/morejellyfish/morejellyfishmod/registry/MoreJellyfishItems.java
@@ -35,7 +35,7 @@ public class MoreJellyfishItems {
     public static final RegistryObject<Item> GUARDIAN_JELLYFISH_SPAWN_EGG = ITEMS.register("guardian_jellyfish_spawn_egg", () -> new MoreJellyfishSpawnEgg(() -> MoreJellyfishEntities.GUARDIAN_JELLYFISH.get(),2640895, 16744495, new Item.Properties().group(ItemGroup.MISC)));
     public static final RegistryObject<Item> SKELLYFISH_SPAWN_EGG = ITEMS.register("skellyfish_spawn_egg", () -> new MoreJellyfishSpawnEgg(() -> MoreJellyfishEntities.SKELLYFISH.get(),13881047, 2236710, new Item.Properties().group(ItemGroup.MISC)));
     public static final RegistryObject<Item> COW_JELLYFISH_SPAWN_EGG = ITEMS.register("cow_jellyfish_spawn_egg", () -> new MoreJellyfishSpawnEgg(() -> MoreJellyfishEntities.COW_JELLYFISH.get(),13948116, 4076073, new Item.Properties().group(ItemGroup.MISC)));
-    public static final RegistryObject<Item> NETHERITE_JELLYFISH_SPAWN_EGG = null;//ITEMS.register("netherite_jellyfish_spawn_egg", () -> new MoreJellyfishSpawnEgg(() -> MoreJellyfishEntities.NETHERITE_JELLYFISH.get(),4668474, 6306874, new Item.Properties().group(ItemGroup.MISC)));
+    public static final RegistryObject<Item> NETHERITE_JELLYFISH_SPAWN_EGG = ITEMS.register("netherite_jellyfish_spawn_egg", () -> new MoreJellyfishSpawnEgg(() -> MoreJellyfishEntities.NETHERITE_JELLYFISH.get(),4668474, 6306874, new Item.Properties().group(ItemGroup.MISC)));
 
 
 
@@ -73,7 +73,7 @@ public class MoreJellyfishItems {
     public static final RegistryObject<Item> SKELLYFISH = ITEMS.register("skellyfish", () -> new JellyfishItem(new Item.Properties().group(ItemGroup.MATERIALS),  () -> MoreJellyfishEntities.SKELLYFISH.get()));
     public static final RegistryObject<Item> COW_JELLYFISH = ITEMS.register("cow_jellyfish", () -> new JellyfishItem(new Item.Properties().group(ItemGroup.MATERIALS),  () -> MoreJellyfishEntities.COW_JELLYFISH.get()));
 
-    public static final RegistryObject<Item> NETHERITE_JELLYFISH = null;//ITEMS.register("netherite_jellyfish", () -> new JellyfishItem(new Item.Properties().group(ItemGroup.MATERIALS),  () -> MoreJellyfishEntities.NETHERITE_JELLYFISH.get()));
+    public static final RegistryObject<Item> NETHERITE_JELLYFISH = ITEMS.register("netherite_jellyfish", () -> new JellyfishItem(new Item.Properties().group(ItemGroup.MATERIALS),  () -> MoreJellyfishEntities.NETHERITE_JELLYFISH.get()));
 
     public static final RegistryObject<Item> HONEY_BALL = ITEMS.register("honey_ball", () -> new Item(new Item.Properties().food(Foods.HONEY).group(ItemGroup.FOOD)));
     public static final RegistryObject<Item> GELATINOUS_MILK = ITEMS.register("gelatinous_milk", () -> new GelatinousMilkItem(new Item.Properties().food(Foods.HONEY).group(ItemGroup.FOOD)));
