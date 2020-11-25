269c6d63d00fc946ecf7bf6360d4227d129b421c
Max <bludux595@gmail.com>
2020-11-24 19:27:18 -0500

    Biome tweaks

--- src/main/java/blueduck/morejellyfish/morejellyfishmod/misc/RockBottom.java
+++ src/main/java/blueduck/morejellyfish/morejellyfishmod/misc/RockBottom.java
@@ -33,7 +33,7 @@ public class RockBottom extends JellyfishingBiome {
 
 
     public RockBottom() {
-        super(CLIMATE, Biome.Category.OCEAN, -1.8F, .25F, (new net.minecraft.world.biome.BiomeAmbience.Builder()).setWaterColor(4159204).setWaterFogColor(329011).setFogColor(12638463).withSkyColor(3448555).setMoodSound(MoodSoundAmbience.DEFAULT_CAVE).build(), GENERATION_SETTINGS.build(), SPAWN_SETTINGS.copy());
+        super(CLIMATE, Biome.Category.OCEAN, -1.8F, .25F, (new net.minecraft.world.biome.BiomeAmbience.Builder()).setWaterColor(4159204).setWaterFogColor(329011).setFogColor(12638463).withSkyColor(7842047).setMoodSound(MoodSoundAmbience.DEFAULT_CAVE).build(), GENERATION_SETTINGS.build(), SPAWN_SETTINGS.copy());
     }
 
 
